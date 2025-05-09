4716c04754dd375821b5b6c28f412c30151ca22d
iretq <me@iretq.dev>
2025-05-09 19:32:20 +0200

    build-support: Modify lint error reporting again

--- build-support/lint.sh
+++ build-support/lint.sh
@@ -7,6 +7,10 @@ errors="$(mktemp)"
 
 trap 'rm -f "$errors"' EXIT
 
+print_error() {
+    printf '\033[31m'"$1"'\033[0m\n'
+}
+
 verify_build_system() {
     # Determine the build system used by the configure step
     name=$(basename "$1")
@@ -21,17 +25,17 @@ verify_build_system() {
         autotools)
             # Check for meson.build or CMakeLists.txt
             if [ -f sources/$name/meson.build ]; then
-                echo "* $1 has meson.build, but uses autotools" >> "$errors"
+                print_error "* $1 has meson.build, but uses autotools" >> "$errors"
                 return 1
             elif [ -f sources/$name/CMakeLists.txt ]; then
-                echo "* $1 has CMakeLists.txt, but uses autotools" >> "$errors"
+                print_error "* $1 has CMakeLists.txt, but uses autotools" >> "$errors"
                 return 1
             fi
             ;;
         cmake)
             # Check for meson.build
             if [ -f sources/$name/meson.build ]; then
-                echo "* $1 has meson.build, but uses cmake" >> "$errors"
+                print_error "* $1 has meson.build, but uses cmake" >> "$errors"
                 return 1
             fi
             ;;
@@ -48,7 +52,7 @@ verify_build_system() {
     # Make sure we're using meson/cmake build/install commands
     # instead of calling ninja directly
     if grep -q 'ninja -C' "$1" || grep -q 'ninja -j' "$1" || grep -q 'ninja install' "$1"; then
-        echo "* $1 uses ninja directly, but should use $build_system" >> "$errors"
+        print_error "* $1 uses ninja directly, but should use $build_system" >> "$errors"
         return 1
     fi
 }
@@ -58,27 +62,27 @@ lint_recipe() {
 
     # Make sure recipe has a shebang
     if ! grep -q '^#! /bin/sh' "$1"; then
-        echo "$1 does not have a shebang" >> "$errors"
+        print_error "$1 does not have a shebang" >> "$errors"
         recipe_has_errors="yes"
     fi
 
     # Check given recipes for bashisms
     bashisms="$(checkbashisms -npfxl "$1")"
     if [ $? -ne 0 ]; then
-        echo "* $1 contains bashisms" >> "$errors"
+        print_error "* $1 contains bashisms" >> "$errors"
         echo "$bashisms" >> "$errors"
         recipe_has_errors="yes"
     fi
 
     # Check for trailing whitespace
     if grep -q '[[:space:]]$' "$1"; then
-        echo "* $1 contains trailing whitespace" >> "$errors"
+        print_error "* $1 contains trailing whitespace" >> "$errors"
         recipe_has_errors="yes"
     fi
 
     # Make sure file is indented with spaces
     if grep -Pq '\t' "$1"; then
-        echo "* $1 contains tabs" >> "$errors"
+        print_error "* $1 contains tabs" >> "$errors"
         recipe_has_errors="yes"
     fi
 
@@ -94,20 +98,19 @@ lint_recipe() {
     fi
 }
 
-for recipe in "$@"; do
+sorted_recipes=$(echo "$@" | xargs -n1 | sort -u | xargs)
+for recipe in $sorted_recipes; do
     printf "* linting $recipe"
 
     if ! lint_recipe "$recipe"; then
-        printf ' \033[31;1mfailed\033[0m\n'
+        printf ' \033[31mfailed\033[0m\n'
         has_errors="yes"
     else
-        printf ' \033[32;1mok\033[0m\n'
+        printf ' \033[32mok\033[0m\n'
     fi
 done
 
 if [ "$has_errors" = "yes" ]; then
-    printf '\033[31;1m'
     cat "$errors"
-    printf '\033[0m'
     exit 1
 fi
